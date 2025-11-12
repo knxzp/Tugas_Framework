4275e19fd5c4e2ba0945623adfab61b8ef056430
kanza <kanza190504@gmail.com>
2025-11-13 00:40:47 +0700

    modul 4

--- warga/urls.py
+++ warga/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView
+from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, WargaUpdateView, WargaDeleteView
 
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('tambah/', WargaCreateView.as_view(), name='warga-tambah'), 
     path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
+    path('<int:pk>/edit/', WargaUpdateView.as_view(), name='warga-edit'),
+    path('<int:pk>/hapus/', WargaDeleteView.as_view(), name='warga-hapus'),
     path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
 
 ]
--- warga/views.py
+++ warga/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm
 
@@ -23,3 +23,14 @@ class PengaduanListView(ListView):
     model = Pengaduan
     template_name = 'warga/warga_list.html'
     context_object_name = 'pengaduan_list'
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html' 
+    success_url = reverse_lazy('warga-list')
+
+class WargaDeleteView(DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga-list')
